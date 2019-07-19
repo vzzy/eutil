418d81ef63290b0e08b8c48d1bf5a674a582c58c
zyzeng <zyzeng@ctrip.com>
2019-07-19 17:03:02 +0800

    新增方法

--- src/eutil.erl
+++ src/eutil.erl
@@ -22,6 +22,7 @@
 	get_timestamp/0,
 	get_micros/0,
 	seconds_to_local_time/1,
+	uuid/0,
 	
 	gc/0,
 	src/1,
@@ -126,6 +127,31 @@ seconds_to_local_time(Seconds)->
 	MegaSecs = Seconds div 1000000,
 	Secs = Seconds rem 1000000,
 	calendar:now_to_local_time({MegaSecs,Secs,0}).
+
+%% UUID生成方法
+%% @return string
+uuid() ->
+    Term = erlang:term_to_binary({node(), make_ref()}),
+    Digest = erlang:md5(Term),
+    binary_to_hex(Digest).
+binary_to_hex(Bin) when is_binary(Bin) ->
+    [oct_to_hex(N) || <<N:4>> <= Bin].
+oct_to_hex(0) -> $0;
+oct_to_hex(1) -> $1;
+oct_to_hex(2) -> $2;
+oct_to_hex(3) -> $3;
+oct_to_hex(4) -> $4;
+oct_to_hex(5) -> $5;
+oct_to_hex(6) -> $6;
+oct_to_hex(7) -> $7;
+oct_to_hex(8) -> $8;
+oct_to_hex(9) -> $9;
+oct_to_hex(10) -> $a;
+oct_to_hex(11) -> $b;
+oct_to_hex(12) -> $c;
+oct_to_hex(13) -> $d;
+oct_to_hex(14) -> $e;
+oct_to_hex(15) -> $f.
 
 
 % 对所有process做gc
